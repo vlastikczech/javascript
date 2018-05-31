96c83cd3311c363651604ef31e56fa8f28294ae0
vlastik <vlastikczech1@gmail.com>
2018-05-30 20:16:35 -0700

    learned how to deal with Subclasses in ES5 & ES6

--- 7-EcmaScript2015/script.js
+++ 7-EcmaScript2015/script.js
@@ -421,3 +421,68 @@ class Person6 {
 var john6 = new Person6('John', 1990, 'designer');
 // class isnt hoisted
 // can only add methods to classes not properties
+
+// Lecture: Classes with Subclasses
+//ES5
+
+var Person5 = function(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person5.prototype.calculateAge = function () {
+    var age = new Date().getFullYear() - this.year;
+    console.log(age);
+}
+
+var Athelete5 = function(name, yearOfBirth, job, olympicGames, medals){
+    Person5.call(this, name, yearOfBirth, job); //this keywords points to the new empty object
+    this.olympicGames = olympicGames;
+    this.medals = medals;
+}
+
+Athelete5.prototype = Object.create(Person5.prototype); // connects the 2 function constructors
+//Athlete5 inherits from Person5
+
+Athelete5.prototype.wonMedal = function() {
+    this.medals++;
+    console.log(this.medals);
+}
+
+
+var johnAthlete5 = new johnAthlete5('John', 1990, 'swimmer', 3, 10);
+johnAthlete5.calculateAge();
+johnAthlete5.wonMedal();
+
+//ES6
+
+class Person6 {
+    constructor (name, yearOfBirth, job){
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.job = job;
+    }
+
+    calculateAge() {
+        var age = new Date().getFullYear() - this.year;
+        console.log(age);
+    }
+}
+
+class Athlete6 extends Person6 { 
+    constructor(name, yearOfBirth, job, olympicGames, medals) {
+        super(name, yearOfBirth, job);;
+        this.olympicGames = olympicGames;
+        this.medals = medals;
+    }
+    
+    wonMedal() {
+        this.medals++
+        console.log(this.medals);
+    }
+}
+
+const johnAthlete6 = new Athelete6('John', 1990, 'swimmer', 3, 10);
+johnAthlete6.wonMedal();
+johnAthlete6.calculateAge();
